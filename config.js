9fc13f5f15f31052e8ee8b1a09cf610ad3166cef
Jay Lee <leejg1313@naver.com>
2016-04-05 03:04:33 +0900

    Add an example
    Auto game bot

---
+++ config.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = {
+	app : {
+		port : 3000,
+		express : {
+			/*!
+			* 클라이언트 리소스를 변경시
+			* static : 'examples/client-auto'
+			*/
+			static : 'client'
+		},
+	},
+};
